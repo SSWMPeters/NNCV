74afb664b21641715622e2a67e40ff28c6c85882
SSWMPeters <134622896+SSWMPeters@users.noreply.github.com>
2025-04-15 15:57:09 +0200

    Change batchsize

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -2,9 +2,9 @@ wandb login
 
 python3 train.py \
     --data-dir ./data/cityscapes \
-    --batch-size 32 \
+    --batch-size 64 \
     --epochs 100 \
-    --lr 0.01 \
+    --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "segformer-training" \
+    --experiment-id "segformer_ood-training" \
